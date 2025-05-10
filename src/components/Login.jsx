e9950e7aa181fbc1dd70dc20dd95779200fee8b7
Bryan1994Espinoza <37449730+Bryan1994Espinoza@users.noreply.github.com>
2025-05-10 17:30:24 -0500

    Estilo Material

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import {useEffect} from "react";
 import {useNavigate} from "react-router";
+import {Grid,Typography} from '@mui/material';
 
 const Login = ({ user }) => {
     const navigate = useNavigate();
@@ -10,7 +11,7 @@ const Login = ({ user }) => {
         const handleInactivity = () => {
             timeoutId = setTimeout(() => {
                 navigate('/libros/todas');
-            }, 5000);
+            }, 5000000);
         };
 
         const handleActivity = () => {
@@ -31,10 +32,27 @@ const Login = ({ user }) => {
     }, [navigate]);
 
     return (
-        <div className="login">
-            <p><strong>Ingrese su usuario:</strong> <input id="usuario" /></p>
-            <p><strong>Ingrese su password:</strong> <input id="passw" /></p>
-        </div>
+
+       <Grid container direction="column" spacing={0} alignItems="center" justifyContent="center"
+        sx={{minHeight: '100vh',backgroundColor: 'primary.main' ,padding: '4'}}>
+            <Grid item className = 'box-shadown'
+            xs={3}
+            sx={{backgroundColor: 'white', padding: '3',borderRadius:2}}>
+            <Typography variant='h5' sx={ {mb:1}}>
+                Inicio Sesión
+            </Typography>
+                <div className="login">
+                    <p><strong>Ingrese su usuario:</strong> <input id="usuario" /></p>
+                    <p><strong>Ingrese su password:</strong> <input id="passw" /></p>
+                </div>
+
+            </Grid>
+
+
+        </Grid>
+
+
+
     );
 };
 
